77d2c8182ba9faec1fe6f00522b89f6749f135a6
Tellez6998 <tellez.03270910@gmail.com>
2023-02-05 18:07:23 -0600

    Componente heroe

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
 })
 
 export class  HeroeComponent {
+    nombre: string = 'Iron Man';
+    edad: number = 45;
 
+    get nombrecapitalizado():string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    CambiarNombre():void{
+        this.nombre='Spider Man'
+    }
+
+    CambiarEdad():void{
+        this.edad = 30;
+    }
 }
